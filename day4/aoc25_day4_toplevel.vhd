ae51bd7e06314deda420d54c776acec73352c26b
p-j-ward <113056043+p-j-ward@users.noreply.github.com>
2026-01-15 22:43:31 +0000

    WIP control logic for toplevel, and a testbench for it

--- day4/aoc25_day4_toplevel.vhd
+++ day4/aoc25_day4_toplevel.vhd
@@ -4,26 +4,33 @@ use ieee.numeric_std.all;
 
 entity aoc25_day4_toplevel is
     generic (
-        DATA_WIDTH  : integer;   -- must be even
-        ADDR_WIDTH  : integer;
-        COUNT_WIDTH : integer
+        DATA_WIDTH  : natural;   -- must be even
+        ADDR_WIDTH  : natural;
+        COUNT_WIDTH : natural
     );
     port(
-        Clk_in      : in  std_logic;
+        Clk_in       : in  std_logic;
 
-        Start_in    : in  std_logic;
+        Start_in     : in  std_logic;
+        Num_lines_in : in  unsigned(COUNT_WIDTH-1 downto 0);
+        Num_cols_in  : in  unsigned(COUNT_WIDTH-1 downto 0);    -- number of words per line
 
         -- External memory interface, to data starting at addr 0
-        Wr_en_in    : in  std_logic;
-        Wr_addr_in  : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
-        Wr_data_in  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
-
-        Rd_addr_in  : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
-        Rd_data_out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+        Rd_addr_out  : out std_logic_vector(ADDR_WIDTH-1 downto 0);
+        Rd_data_in   : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+        Wr_en_out    : out std_logic;
+        Wr_addr_out  : out std_logic_vector(ADDR_WIDTH-1 downto 0);
+        Wr_data_out  : out std_logic_vector(DATA_WIDTH-1 downto 0)
     );
 end entity;
 
 architecture rtl of aoc25_day4_toplevel is
+    -- control signals
+    type control_state_t is (IDLE, INIT_CACHES, START_PASS, PASS_IN_PROGRESS, POST_PAD, WAIT_FOR_PIPELINE_DONE);
+    signal control_state : control_state_t := IDLE;
+    signal line_idx, col_idx : unsigned(COUNT_WIDTH-1 downto 0) := (others => '0'); -- line and column indicies, these are used for internal control, and adressing external ram
+    signal post_ctr : integer range 0 to 2 + DATA_WIDTH := 0;
+
     -- input to pipeline, we have three words: left, centre, and right; where
     -- left is from the oldest cache, centre the newest cache, and right the
     -- data currently being read from the external memory - critically the
@@ -49,8 +56,9 @@ architecture rtl of aoc25_day4_toplevel is
     signal cache_a_rd_addr, cache_b_rd_addr : std_logic_vector(ADDR_WIDTH-1 downto 0) := (others => '0');
     signal cache_a_rd_data, cache_b_rd_data : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal write_to_cache_a_not_b : std_logic := '0';   -- when this is high, cache a is oldest, when low, cache b is oldest
-begin
 
+    
+begin
     pipeline_inst : entity work.conv_count_update_pipeline
     generic map (
         PIPELINE_DEPTH => DATA_WIDTH,
@@ -101,5 +109,113 @@ begin
         Rd_addr_in  => cache_b_rd_addr,
         Rd_data_out => cache_b_rd_data
     );
+
+    -- We make passes going down the file, line by line, after each pass we can
+    -- move one column across, the sequency for one pass down is as follows:
+    --   1. Initialse by zeroing cache a (right) and loading first column (hence
+    --      column 1) of input data into cache b (centre).
+    --   2. First pass down: input to pipeline is [cache a, cache b, memory]
+    --      where memory is the external memory (data column 2 in this case).
+    --      Simultaneously we load memory into cache a. The output of the
+    --      pipeline for this step will be written back to memory column 1.
+    --   3. Second pass down: input to pipeline is [cache b, cache a, memory],
+    --      recall cache b contains column 1, cache a contains column 2, and the
+    --      pipeline output will be written back to column 2. Simultaneously we
+    --      load memory into cache b.
+    --   4. Repeat steps 2 and 3, moving one column across the input with each
+    --      pass down, and ping-ponging the caches.
+    control_proc : process(Clk_in)
+    begin
+        if rising_edge(Clk_in) then
+            pipeline_dv_in <= '0';  -- may not be wanted in this proc
+
+            case control_state is
+                when IDLE =>
+                    col_idx  <= (others => '0');
+                    line_idx <= (others => '0');
+                    post_ctr <= 0;
+                    if Start_in = '1' then
+                        control_state <= INIT_CACHES;
+                    end if;
+
+                when INIT_CACHES => -- writes zeros to cache a and mem(col 1) to cache b
+                    line_idx <= line_idx + 1;
+                    if line_idx = Num_lines_in then
+                        control_state <= START_PASS;
+                    end if;
+
+                when START_PASS =>  -- adds a line of zero padding at the start
+                    pipeline_dv_in <= '1';
+                    line_idx <= (others => '0');
+                    control_state <= PASS_IN_PROGRESS;
+
+                when PASS_IN_PROGRESS =>  -- main processing, feeding data into pipeline
+                    line_idx <= line_idx + 1;
+                    if line_idx = Num_lines_in then
+                        control_state <= POST_PAD;
+                    end if;
+
+                when POST_PAD => -- add 1 + pipeline depth zero padding at the end, recall pipeline depth is DATA_WIDTH
+                    post_ctr <= post_ctr + 1;
+                    if post_ctr >= 1 + DATA_WIDTH then
+                        control_state <= WAIT_FOR_PIPELINE_DONE;
+                    end if;
+
+                when WAIT_FOR_PIPELINE_DONE =>
+                    if pipeline_dv_out = '0' then
+                        control_state <= IDLE;  -- for test only, let's see how this goes......
+                    end if;
+            end case;
+        end if;
+    end process;
+
+    cache_input_mux_proc : process(Clk_in)
+    begin
+        if rising_edge(Clk_in) then
+            case control_state is
+                when INIT_CACHES =>
+                    cache_a_wr_data <= (others => '0');
+                    cache_b_wr_data <= Rd_data_in;  --- TODO figure out timing etc
+                    cache_a_wr_en <= '1';
+                    cache_b_wr_en <= '1';
+
+                when PASS_IN_PROGRESS =>
+                    if write_to_cache_a_not_b = '1' then
+                        cache_a_wr_data <= Rd_data_in;
+                        cache_b_wr_data <= (others => '0');
+                        cache_a_wr_en <= '1';
+                        cache_b_wr_en <= '0';
+                    else
+                        cache_a_wr_data <= (others => '0');
+                        cache_b_wr_data <= Rd_data_in;
+                        cache_a_wr_en <= '0';
+                        cache_b_wr_en <= '1';
+                    end if;
+
+                when others =>
+                    cache_a_wr_data <= (others => '0');
+                    cache_b_wr_data <= (others => '0');
+                    cache_a_wr_en <= '0';
+                    cache_b_wr_en <= '0';
+            end case;
+        end if;
+    end process;
+
+    -- cache_ping_pong_proc : process (Clk_in)
+    -- begin
+    --     if rising_edge(Clk_in) then
+    --         if write_to_cache_a_not_b = '1' then
+    --             left_word_to_pipeline   <= cache_a_rd_data;
+    --             centre_word_to_pipeline <= cache_b_rd_data;
+    --         else
+    --             left_word_to_pipeline   <= cache_b_rd_data;
+    --             centre_word_to_pipeline <= cache_a_rd_data;
+    --         end if;
+    --         right_word_to_pipeline <= Rd_data_in;
+    --         ---- TODO: pipeline dv, figure out timing and such..... maybe not here though
+    --     end if;
+    -- end process;
+
+
 
 end architecture;
--- day4/test_aoc25_day4_toplevel.vhd
+++ day4/test_aoc25_day4_toplevel.vhd
@@ -1,7 +1,69 @@
 -- Top level testbench for aoc25_day4_toplevel
 --
 -- To run this testbench, with a terminal in day4 directory, run:
---   ghdl -a --std=08 aoc25_day4_pkg.vhd bit_convolution_2d.vhd conv_count_update_step.vhd conv_count_update_pipeline.vhd simple_dual_port_ram.vhd aoc25_day4_toplevel.vhd
+--   ghdl -a --std=08 aoc25_day4_pkg.vhd bit_convolution_2d.vhd conv_count_update_step.vhd conv_count_update_pipeline.vhd simple_dual_port_ram.vhd aoc25_day4_toplevel.vhd test_aoc25_day4_toplevel.vhd
 --   ghdl -e --std=08 test_aoc25_day4_toplevel
 --   ghdl -r --std=08 test_aoc25_day4_toplevel --wave=test_aoc25_day4_toplevel.ghw
 --
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library std;
+use std.textio.all;
+
+use work.aoc25_day4_pkg.all;
+
+entity test_aoc25_day4_toplevel is
+end entity;
+
+architecture testbench of test_aoc25_day4_toplevel is
+    -- clock
+    constant NUM_SIM_CYCLES : natural := 100;
+    constant T_WAIT : time := 1 ns;
+    signal clk : std_logic := '0';
+
+    -- signals for dut
+    constant DATA_WIDTH : natural := 4;
+    constant ADDR_WIDTH : natural := 8;
+    signal rd_addr_out, wr_addr_out : std_logic_vector(ADDR_WIDTH-1 downto 0);
+    signal rd_data_in,  wr_data_out : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal wr_en_out : std_logic;
+
+    signal start : std_logic := '0';
+begin
+    dut : entity work.aoc25_day4_toplevel
+    generic map (
+        DATA_WIDTH  => DATA_WIDTH,
+        ADDR_WIDTH  => ADDR_WIDTH,
+        COUNT_WIDTH => 16
+    )
+    port map (
+        Clk_in       => clk,
+
+        Start_in     => start,
+        Num_lines_in => x"000A",
+        Num_cols_in  => x"000A",
+
+        -- External memory interface, to data starting at addr 0
+        Rd_addr_out  => rd_addr_out,
+        Rd_data_in   => rd_data_in,
+        Wr_en_out    => wr_en_out,
+        Wr_addr_out  => wr_addr_out,
+        Wr_data_out  => wr_data_out
+    );
+
+    clock_proc : process
+    begin
+        for i in 1 to NUM_SIM_CYCLES loop
+            clk <= '0';
+            wait for T_WAIT;
+            clk <= '1';
+            wait for T_WAIT;
+        end loop;
+        wait;
+    end process;
+
+    start <= '1' after 10*T_WAIT, '0' after 12*T_WAIT;
+
+end architecture;
